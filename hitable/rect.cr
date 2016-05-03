105bef0dff05fae3254c47b57498c6e8f154215a
Leon Rische <leon.rische@student.uni-siegen.de>
2016-05-03 23:08:10 +0200

    Add XZ and YZ rects

--- hitable/rect.cr
+++ hitable/rect.cr
@@ -1,22 +1,29 @@
 class XYRect < Hitable
   property x0, x1, y0, y1, z, material
 
-  def initialize(@x0, @x1, @y0, @y1, @z, @material)
+  def initialize(bottom, top, @material)
+    if bottom.z != top.z
+      raise "XYRect bottom & top don't have the same z value"
+    end
+
+    @x0 = bottom.x
+    @x1 = top.x
+    @y0 = bottom.y
+    @y1 = top.y
+    @z = bottom.z
   end
 
   def hit(ray, t_min, t_max)
-    t = (z - ray.origin.z) / ray.direction.z
-
+    t = (@z - ray.origin.z) / ray.direction.z
     return nil if t < t_min || t > t_max
 
     point = ray.point_at_parameter(t)
 
     # Hitpoint is outside of the rect
-    return nil if point.x < x0 || point.x > x1 ||
-                  point.y < y0 || point.y > y1
+    return nil if point.x < @x0 || point.x > @x1 ||
+                  point.y < @y0 || point.y > @y1
 
     normal = Vec3.new(0.0, 0.0, 1.0)
-
     return Intersection.new(t, point, normal, @material)
   end
 
@@ -27,3 +34,74 @@ class XYRect < Hitable
   end
 end
 
+class XZRect < Hitable
+  property x0, x1, y, z0, z1, material
+
+  def initialize(bottom, top, @material)
+    if bottom.y != top.y
+      raise "XZRect bottom & top don't have the same y value"
+    end
+
+    @x0 = bottom.x
+    @x1 = top.x
+    @y = bottom.y
+    @z0 = bottom.z
+    @z1 = top.z
+  end
+
+  def hit(ray, t_min, t_max)
+    t = (@y - ray.origin.y) / ray.direction.y
+    return nil if t < t_min || t > t_max
+
+    point = ray.point_at_parameter(t)
+
+    # Hitpoint is outside of the rect
+    return nil if point.x < @x0 || point.x > @x1 ||
+                  point.z < @z0 || point.z > @z1
+
+    normal = Vec3.new(0.0, 1.0, 0.0)
+    return Intersection.new(t, point, normal, @material)
+  end
+
+  def bounding_box
+    bottom = Vec3.new(@x0, @y-0.0001, @z0)
+    top = Vec3.new(@x1, @y+0.0001, @z1)
+    AABB.new(bottom, top)
+  end
+end
+
+class YZRect < Hitable
+  property x, y0, y1, z0, z1, material
+
+  def initialize(bottom, top, @material)
+    if bottom.x != top.x
+      raise "YZRect bottom & top don't have the same x value"
+    end
+
+    @x = bottom.x
+    @y0 = bottom.y
+    @y1 = top.y
+    @z0 = bottom.z
+    @z1 = top.z
+  end
+
+  def hit(ray, t_min, t_max)
+    t = (@x - ray.origin.x) / ray.direction.x
+    return nil if t < t_min || t > t_max
+
+    point = ray.point_at_parameter(t)
+
+    # Hitpoint is outside of the rect
+    return nil if point.y < @y0 || point.y > @y1 ||
+                  point.z < @z0 || point.z > @z1
+
+    normal = Vec3.new(1.0, 0.0, 0.0)
+    return Intersection.new(t, point, normal, @material)
+  end
+
+  def bounding_box
+    bottom = Vec3.new(@x-0.0001, @y0, @z0)
+    top = Vec3.new(@x+0.0001, @y1, @z1)
+    AABB.new(bottom, top)
+  end
+end
