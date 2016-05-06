eac55da73727bc05a96d13370d5e36253bd7eae4
Leon Rische <leon.rische@student.uni-siegen.de>
2016-05-07 00:16:23 +0200

    Use stratified random rays

--- raytracer.cr
+++ raytracer.cr
@@ -22,20 +22,31 @@ class Raytracer
     file.puts "#{width} #{height}"
     file.puts "255"
 
+    samples_sqrt = Math.sqrt(samples).ceil
+
     (0...@height).reverse_each do |y|
       (0...@width).each do |x|
         col = Vec3.new(0.0)
-        samples.times do
-          u = (x + rand).to_f / @width
-          v = (y + rand).to_f / @height
 
-          ray = camera.get_ray(u, v)
+        (0...samples_sqrt).each do |i|
+          (0...samples_sqrt).each do |j|
+            off_x = (i + rand) / samples_sqrt
+            off_y = (j + rand) / samples_sqrt
 
-          col += color(ray, world)
+            u = (x + off_x).to_f / @width
+            v = (y + off_y).to_f / @height
+
+            ray = camera.get_ray(u, v)
+
+            col += color(ray, world)
+          end
         end
-        col /= samples.to_f
+
+        col /= (samples_sqrt * samples_sqrt)
         col *= 255.99
 
+        # Cap color values at 255 so that lights (albedo > 1.0)
+        # do not lead to corrupted ppm files
         file.print "#{min(col.x.to_i, 255)} "
         file.print "#{min(col.y.to_i, 255)} "
         file.print "#{min(col.z.to_i, 255)}\n"
