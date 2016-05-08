a19c4b0230067f1b24260256c416e735634bab2c
Leon Rische <leon.rische@student.uni-siegen.de>
2016-05-08 04:11:35 +0200

    Fix AABB hit code

--- aabb.cr
+++ aabb.cr
@@ -4,22 +4,22 @@ class AABB
   def initialize(@min : Vec3, @max : Vec3)
   end
 
-  def hit(ray, tmin, tmax)
-    min_ = @min.xyz
-    max_ = @max.xyz
+  def hit(ray)
+    dirfrac = Vec3.new(1.0) / ray.direction
 
-    origin = ray.origin.xyz
-    dir = ray.direction.xyz
+    t1 = (@min.x - ray.origin.x) * dirfrac.x
+    t2 = (@max.x - ray.origin.x) * dirfrac.x
+    t3 = (@min.y - ray.origin.y) * dirfrac.y
+    t4 = (@max.y - ray.origin.y) * dirfrac.y
+    t5 = (@min.z - ray.origin.z) * dirfrac.z
+    t6 = (@max.z - ray.origin.z) * dirfrac.z
 
-    (0...3).each do |a|
-      t0, t1 = minmax(min_[a] - origin[a] / dir[a],
-                      max_[a] - origin[a] / dir[a])
+    tmin = max(max(min(t1, t2), min(t3, t4)), min(t5, t6))
+    tmax = min(min(max(t1, t2), max(t3, t4)), max(t5, t6))
 
-      tmin = max(t0, tmin)
-      tmax = min(t1, tmax)
-
-      return false if tmax <= tmin
-    end
+    return false if tmax < 0
+    return false if tmin > tmax
+    return true
   end
 
   def merge(other)
--- hitable/bvh.cr
+++ hitable/bvh.cr
@@ -1,33 +1,37 @@
 # Bounding volume hierarchy
 
 class BVHNode < Hitable
-  getter left, right, bounding_box : AABB
+  getter left : Hitable
+  getter right : Hitable
+  getter bounding_box : AABB
 
-  def initialize(list)
-    axis = rand(0.0...3.0).to_i
+  def initialize(list, axis = 0)
     sorted = list.sort_by { |h| h.box_min_on_axis(axis) }
 
     n = sorted.size
+
     if n == 1
       @left = @right = sorted[0]
-    elsif n == 2
-      @left = sorted[0]
-      @right = sorted[1]
+    elsif n <= 64
+      half = n / 2
+      @left = HitableList.new(sorted[0...half])
+      @right = HitableList.new(sorted[half..-1])
     else
       half = n / 2
-      @left = BVHNode.new(sorted[0...half])
-      @right = BVHNode.new(sorted[half..-1])
+      @left = BVHNode.new(sorted[0...half], (axis + 1) % 3)
+      @right = BVHNode.new(sorted[half..-1], (axis + 1) % 3)
     end
 
     @bounding_box = @left.bounding_box.merge(@right.bounding_box)
   end
 
   def hit(ray, t_min, t_max)
-    if @bounding_box.hit(ray, t_min, t_max)
+    if @bounding_box.hit(ray)
       hit_left = @left.hit(ray, t_min, t_max)
       hit_right = @right.hit(ray, t_min, t_max)
 
       if (hit_left && hit_right)
+        @double += 1
         if hit_left.t < hit_right.t
           return hit_left
         else
@@ -40,6 +44,8 @@ class BVHNode < Hitable
       else
         return nil
       end
+    else
+      return nil
     end
   end
 end
