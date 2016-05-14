20266f55cfe366b0a2f9aa902cafa1f404cacf86
Leon Rische <leon.rische@student.uni-siegen.de>
2016-05-14 21:39:56 +0200

    Add function to read ppm files into Vec3 arrays

--- ppm.cr
+++ ppm.cr
@@ -1,3 +1,5 @@
+require "./vec3"
+
 class PPM
   def initialize(width, height, filename)
     @file = File.open(filename, "w")
@@ -18,5 +20,25 @@ class PPM
     # Make sure color values are valid (0..255)
     rgb = color.xyz.map { |v| min(max(v.to_i, 0), 255) }
     @file.puts rgb.join(" ")
+  end
+
+  def self.read(filename)
+    lines = File.read(filename).lines.map(&.chomp)
+
+    type = lines[0]
+    dimensions = lines[1].split.map(&.to_i) 
+    depth = lines[2]
+
+    raise "Invalid ppm file format #{type}" unless type == "P3"
+    raise "Invalid ppm depth #{depth}" unless depth == "255" # TODO: Support depths other than 255
+
+    # Convert file into one long list of integers
+    raw_body = lines[3..-1].map { |line| line.split.map(&.to_i) }.flatten
+
+    # Create color vectors from each integer triple (r, g, b)
+    raw = raw_body.each_slice(3).map { |rgb| Vec3.new(rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0) }
+
+    # "Restore" the original width
+    raw.each_slice(dimensions[0]).to_a
   end
 end
