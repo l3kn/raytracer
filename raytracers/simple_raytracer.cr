3276441c63f51325b27db3209f2d557a48596792
Leon Rische <leon.rische@student.uni-siegen.de>
2016-05-17 22:26:23 +0200

    Add debug mode (color = hit.normal)

--- raytracers/simple_raytracer.cr
+++ raytracers/simple_raytracer.cr
@@ -1,18 +1,22 @@
 require "../raytracer"
 
 class SimpleRaytracer < Raytracer
-  def initialize(width, height, world, camera, samples)
+  def initialize(width, height, world, camera, samples, @debug = false)
     super(width, height, world, camera, samples)
   end
 
   def color(ray, world, recursion_level = 0)
     hit = world.hit(ray, 0.0001, 9999.9)
     if hit
-      scatter = hit.material.scatter(ray, hit)
-      if scatter && recursion_level < RECURSION_LIMIT
-        scatter.albedo * color(scatter.ray, world, recursion_level + 1)
+      if @debug
+        (hit.normal + Vec3.new(1.0)) / 2
       else
-        Vec3.new(0.0)
+        scatter = hit.material.scatter(ray, hit)
+        if scatter && recursion_level < RECURSION_LIMIT
+          scatter.albedo * color(scatter.ray, world, recursion_level + 1)
+        else
+          Vec3.new(0.0)
+        end
       end
     else
       # "Sky" color
