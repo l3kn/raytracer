23241daec3f90a904b3210df8df38509316de52f
Leon Rische <leon.rische@student.uni-siegen.de>
2016-06-07 20:42:52 +0200

    Put step size in constructor

--- distance_estimator.cr
+++ distance_estimator.cr
@@ -4,10 +4,11 @@ module DE
   class DistanceEstimator < Hitable
     property material : Material
     property object : DistanceEstimatable
+    property step : Float64
 
     MAXIMUM_RAY_STEPS = 1000
 
-    def initialize(@material, @object)
+    def initialize(@material, @object, @step = 0.1)
     end
 
     def hit(ray, t_min, t_max)
@@ -29,16 +30,15 @@ module DE
         break if distance < 0.0001
       end
 
-      step = 0.0001
-      x_dir = Vec3.new(step, 0.0, 0.0)
-      y_dir = Vec3.new(0.0, step, 0.0)
-      z_dir = Vec3.new(0.0, 0.0, step)
+      x_dir = Vec3.new(@step, 0.0,   0.0)
+      y_dir = Vec3.new(0.0,   @step, 0.0)
+      z_dir = Vec3.new(0.0,   0.0,   @step)
 
       normal = Vec3.new(distance_estimate(point + x_dir) - distance_estimate(point - x_dir),
                         distance_estimate(point + y_dir) - distance_estimate(point - y_dir),
                         distance_estimate(point + z_dir) - distance_estimate(point - z_dir)).normalize
 
-      return ::Intersection.new(total_distance, point, normal, @material, u: 0.0, v: 0.0)
+      return ::Intersection.new(total_distance, point + normal * 2 * t_min, normal, @material, u: 0.0, v: 0.0)
     end
 
     def distance_estimate(pos)
