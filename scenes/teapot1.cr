f1988f800c054526d8f5cdbded68143f59fa3cc0
Leon Rische <leon.rische@student.uni-siegen.de>
2016-08-08 15:13:05 +0200

    Interpolate triangle normals

--- scenes/teapot1.cr
+++ scenes/teapot1.cr
@@ -1,8 +1,9 @@
 require "../src/raytracer"
 require "../src/backgrounds/*"
 
-ct2 = ConstantTexture.new(Vec3.new(0.1, 0.2, 0.5))
+ct2 = ConstantTexture.new(Vec3.new(0.9))
 mat = Lambertian.new(ct2)
+# mat = Metal.new(ct2, 0.0)
 
 world = [] of Hitable
 
@@ -23,20 +24,8 @@ obj.lines.each do |line|
     normals << Vec3.new(cords[0], cords[1], cords[2])
   when "f"
     a, b, c = tokens[1, 3].map { |i| vertices[i.split("//")[0].to_i - 1] }
-    t1 = Triangle.new(a, b, c, mat)
-    t2 = Triangle.new(a, c, b, mat)
-
     na, nb, nc = tokens[1, 3].map { |i| normals[i.split("//")[1].to_i - 1] }
-    vnormal = ((na + nb + nc) / 3.0).normalize
-
-    d1 = (t1.normal - vnormal).length
-    d2 = (t2.normal - vnormal).length
-
-    if d1 < d2
-      world << t1
-    else
-      world << t2
-    end
+    world << InterpolatedTriangle.new(a, b, c, na, nb, nc, mat)
   end
 end
 
@@ -44,14 +33,14 @@ puts "Parsed #{vertices.size} vertices"
 puts "Parsed #{vertices.size} normals"
 puts "Parsed #{world.size} faces"
 
-width, height = {800, 400}
+width, height = {400, 400}
 
 # Camera params
-look_from = Vec3.new(-1.5, 1.5, 2.5)
-look_at = Vec3.new(0.0, 0.0, -1.0)
+look_from = Vec3.new(-1.5, 1.5, -2.0)
+look_at = Vec3.new(0.0, 0.5, 0.0)
 
 up = Vec3.new(0.0, 1.0, 0.0)
-fov = 30
+fov = 40
 
 aspect_ratio = width.to_f / height.to_f
 dist_to_focus = (look_from - look_at).length
@@ -63,7 +52,9 @@ camera = Camera.new(look_from, look_at, up, fov, aspect_ratio, aperture, dist_to
 raytracer = Raytracer.new(width, height,
                           world: BVHNode.new(world),
                           camera: camera,
-                          samples: 50,
+                          samples: 100,
+                          # background: CubeMap.new("cube_maps/Yokohama"))
                           background: SkyBackground.new)
+                          # background: ConstantBackground.new(Vec3.new(1.0, 0.0, 0.0)))
 
 raytracer.render("teapot1.png")
---
+++ src/hitables/interpolated_triangle.cr
@@ -0,0 +1,91 @@
+class InterpolatedTriangle < Hitable
+  getter bounding_box
+  # Vertices
+  getter a : Vec3
+  getter b : Vec3
+  getter c : Vec3
+  # Vertex normals
+  getter na : Vec3
+  getter nb : Vec3
+  getter nc : Vec3
+  property material : Material
+
+  getter edge1 : Vec3
+  getter edge2 : Vec3
+
+  def initialize(@a, @b, @c, @na, @nb, @nc, @material)
+    min = Vec3.new(
+      [@a.x, @b.x, @c.x].min,
+      [@a.y, @b.y, @c.y].min,
+      [@a.z, @b.z, @c.z].min
+    )
+
+    max = Vec3.new(
+      [@a.x, @b.x, @c.x].max,
+      [@a.y, @b.y, @c.y].max,
+      [@a.z, @b.z, @c.z].max
+    )
+
+    @bounding_box = AABB.new(min, max)
+
+    @edge1 = @b - @a
+    @edge2 = @c - @a
+  end
+
+  EPSILON = 0.000001
+
+  # Möller-Trumbore intersection algorithm
+  def hit(ray, t_min, t_max)
+    p = ray.direction.cross(@edge2)
+    det = @edge1.dot(p)
+
+    # Ray lies in the plane of the triangle
+    return nil if det > -EPSILON && det < EPSILON
+
+    inv_det = 1.0 / det
+
+    t = ray.origin - @a
+    u = t.dot(p) * inv_det
+
+    # The intersection lies outside of the triangle
+    return nil if u < 0.0 || u > 1.0
+
+    q = t.cross(@edge1)
+    v = ray.direction.dot(q) * inv_det
+
+    # The intersection lies outside of the triangle
+    return nil if v < 0.0 || (u + v) > 1.0
+
+    t = @edge2.dot(q) * inv_det
+
+    if (t < t_max && t > t_min)
+      point = ray.point_at_parameter(t)
+      bc = barycentricCoordinates(point)
+      normal = @na * bc.x + @nb * bc.y + @nc * bc.z
+      return Intersection.new(t, point, normal, @material, u, v)
+    else
+      return nil
+    end
+  end
+
+  def barycentricCoordinates(p)
+    v0 = @b - @a
+    v1 = @c - @a
+    v2 = p - @a
+
+    d00 = v0.dot(v0)
+    d01 = v0.dot(v1)
+    d11 = v1.dot(v1)
+    d20 = v2.dot(v0)
+    d21 = v2.dot(v1)
+
+    denom = d00 * d11 - d01 * d01
+
+    v = (d11 * d20 - d01 * d21) / denom
+    w = (d00 * d21 - d01 * d20) / denom
+    u = 1.0 - v - w
+
+    Vec3.new(u, v, w)
+  end
+end
+
