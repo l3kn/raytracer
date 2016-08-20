7fc48e4dedaf06f007e16e616be598fe8e5b68fe
Leon Rische <leon.rische@student.uni-siegen.de>
2016-08-20 16:55:01 +0200

    Add helper hitables

---
+++ src/hitables/translate.cr
@@ -0,0 +1,30 @@
+class Translate < Hitable
+  getter offset : Vec3
+  getter object : Hitable
+  getter bounding_box : AABB
+
+  def initialize(@object, @offset)
+    @bounding_box = AABB.new(
+      @object.bounding_box.min + @offset,
+      @object.bounding_box.max + @offset,
+    )
+  end
+  
+  def hit(ray, t_min, t_max)
+    moved_ray = Ray.new(ray.origin - @offset, ray.direction)
+
+    hit = @object.hit(moved_ray, t_min, t_max)
+    if hit
+      Intersection.new(
+        hit.t,
+        hit.point + @offset,
+        hit.normal,
+        hit.material,
+        hit.u,
+        hit.v
+      )
+    else
+      nil
+    end
+  end
+end
