e5fd6613bd8ceb917e8b1cadc38ca6456e45a994
Leon Rische <leon.rische@student.uni-siegen.de>
2016-08-20 16:58:24 +0200

    Add cornell box scene

---
+++ scenes/cornell_box.cr
@@ -0,0 +1,83 @@
+require "../src/raytracer"
+
+tex_white = ConstantTexture.new(Vec3.new(0.73))
+tex_red   = ConstantTexture.new(Vec3.new(0.65, 0.05, 0.05))
+tex_green = ConstantTexture.new(Vec3.new(0.12, 0.45, 0.15))
+
+tex_light = ConstantTexture.new(Vec3.new(15.0))
+
+white = Lambertian.new(tex_white)
+red   = Lambertian.new(tex_red)
+green = Lambertian.new(tex_green)
+
+light = DiffuseLight.new(tex_light)
+
+left   = YZRect.new(Vec3.new(555.0, 0.0, 0.0),
+                    Vec3.new(555.0, 555.0, 555.0),
+                    green)
+left.flip!
+
+right  = YZRect.new(Vec3.new(0.0, 0.0, 0.0),
+                    Vec3.new(0.0, 555.0, 555.0),
+                    red)
+
+bottom = XZRect.new(Vec3.new(0.0, 0.0, 0.0),
+                    Vec3.new(555.0, 0.0, 555.0),
+                    white)
+
+top    = XZRect.new(Vec3.new(0.0, 555.0, 0.0),
+                    Vec3.new(555.0, 555.0, 555.0),
+                    white)
+top.flip!
+
+back   = XYRect.new(Vec3.new(0.0, 0.0, 555.0),
+                    Vec3.new(555.0, 555.0, 555.0),
+                    white)
+back.flip!
+
+light_ = XZRect.new(Vec3.new(213.0, 554.0, 227.0),
+                    Vec3.new(343.0, 554.0, 332.0),
+                    light)
+light_.flip!
+
+cube1 = Translate.new(
+  RotateY.new(
+    Cuboid.new(Vec3.new(0.0), Vec3.new(165.0), white),
+    -18.0
+  ),
+  Vec3.new(130.0, 0.0, 65.0)
+)
+
+cube2 = Translate.new(
+  RotateY.new(
+    Cuboid.new(Vec3.new(0.0), Vec3.new(165.0, 330.0, 165.0), white),
+    15.0
+  ),
+  Vec3.new(265.0, 0.0, 295.0)
+)
+
+world = HitableList.new([left, right, bottom, top, back, light_, cube1, cube2])
+
+width, height = {400, 400}
+
+# Camera params
+look_from = Vec3.new(278.0, 278.0, -800.0)
+look_at = Vec3.new(278.0, 278.0, 0.0)
+
+up = Vec3.new(0.0, 1.0, 0.0)
+
+aspect_ratio = width.to_f / height.to_f
+dist_to_focus = 10.0
+aperture = 0.05
+fov = 40
+
+camera = Camera.new(look_from, look_at, up, fov, aspect_ratio, aperture, dist_to_focus)
+
+# Raytracer
+raytracer = Raytracer.new(width, height,
+                          world: world,
+                          camera: camera,
+                          samples: 10,
+                          background: ConstantBackground.new(Vec3.new(0.0)))
+
+raytracer.render("cornell.png")
