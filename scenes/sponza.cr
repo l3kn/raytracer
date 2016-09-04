c9637c87a7e748317f02563c632a9d5087209e76
Leon Rische <leon.rische@student.uni-siegen.de>
2016-09-04 18:58:53 +0200

    Add surface area heuristic aware BVH and the sponza scene

---
+++ scenes/sponza.cr
@@ -0,0 +1,111 @@
+require "../src/raytracer"
+require "../src/backgrounds/*"
+require "../src/obj"
+
+ct2 = ConstantTexture.new(Vec3.new(0.0, 0.0, 1.0))
+mat = Lambertian.new(ct2)
+
+def get_texture(name)
+  # The textures are to big to include them in this repo,
+  # download them here: http://www.crytek.com/cryengine/cryengine3/downloads
+  # and convert all .tga files to .png
+  # e.g. using `mogrify -format png *.tga
+  ImageTexture.new("old/models/sponza/textures/#{name}.png")
+end
+
+textures = {
+  "leaf" => get_texture("thorn"),
+  "vase_round" => get_texture("vase_round"),
+  "Material__57" => get_texture("vase_plant"),
+  "Material__298" => get_texture("background"),
+  "16___Default" => ConstantTexture.new(Vec3.new(0.5880)),
+  "bricks" => get_texture("bricks"),
+  "arch" => get_texture("sponza_arch_diff"),
+  "column_a" => get_texture("sponza_column_a_diff"),
+  "column_b" => get_texture("sponza_column_b_diff"),
+  "column_c" => get_texture("sponza_column_c_diff"),
+  "floor" => get_texture("sponza_floor_a_diff"),
+  "details" => get_texture("sponza_details_diff"),
+  "Material__47" => ConstantTexture.new(Vec3.new(0.5880)),
+  "flagpole" => get_texture("sponza_flagpole_diff"),
+  "fabric_e" => get_texture("sponza_fabric_green_diff"),
+  "fabric_d" => get_texture("sponza_fabric_blue_diff"),
+  "fabric_a" => get_texture("sponza_fabric_diff"),
+  "fabric_g" => get_texture("sponza_curtain_blue_diff"),
+  "fabric_c" => get_texture("sponza_curtain_diff"),
+  "fabric_f" => get_texture("sponza_curtain_green_diff"),
+  "chain" => get_texture("chain_texture"),
+  "vase_hanging" => get_texture("vase_hanging"),
+  "vase" => get_texture("vase_dif"),
+  "Material__25" => get_texture("lion"),
+  "roof" => get_texture("sponza_roof_diff"),
+  "ceiling" => get_texture("sponza_ceiling_a_diff"),
+}
+
+hitables = OBJ.parse("old/models/sponza/sponza.obj", mat, interpolated: true, textured: true, textures: textures)
+
+light = XZRect.new(
+  Vec3.new(-900.0, 1800.0, -80.0),
+  Vec3.new(900.0, 1800.0, 80.0),
+  DiffuseLight.new(Vec3::ONE, 40.0),
+)
+hitables << light
+
+# width, height = {1920, 1080}
+width, height = 400, 400
+node = SAHBVHNode.new(hitables)
+
+
+look_from = Vec3.new(940.0, 600.0, 80.0)
+look_at = Vec3.new(0.0, 400.0, 0.0)
+up = Vec3::Y
+vertical_fov = 70
+aspect_ratio = width.to_f / height.to_f
+
+camera = Camera.new(
+  look_from, look_at, up, vertical_fov, aspect_ratio,
+  aperture: 0.0
+)
+
+raytracer = Raytracer.new(width, height,
+                          hitables: node,
+                          focus_hitables: light,
+                          camera: camera,
+                          samples: 5000,
+                          background: ConstantBackground.new(Vec3.new(1.0)))
+
+raytracer.recursion_depth = 4
+raytracer.render("sponza.png")
+
+# "Interactive" mode for finding a good camera position
+# while true
+#   print "> "
+#   command = gets || ""
+#   next if command.empty?
+
+#   tokens = command.chomp.split
+#   case tokens[0]
+#   when "quit"
+#     break
+#   when "look_from"
+#     x, y, z = tokens[1,3].map(&.to_f)
+#     look_from = Vec3.new(x, y, z)
+#   when "look_at"
+#     x, y, z = tokens[1,3].map(&.to_f)
+#     look_at = Vec3.new(x, y, z)
+#   when "size"
+#     width, height = tokens[1,2].map(&.to_i)
+#   when "render"
+#     camera = Camera.new(
+#       look_from, look_at, up, vertical_fov, aspect_ratio,
+#       aperture: 0.0
+#     )
+#     raytracer = NormalRaytracer.new(width, height,
+#       hitables: node,
+#       camera: camera,
+#       samples: 10,
+#       background: ConstantBackground.new(Vec3.new(1.0)))
+#     raytracer.render("sponza.png")
+#     puts ""
+#   end
+# end
---
+++ src/hitables/sah_bvh.cr
@@ -0,0 +1,125 @@
+require "../aabb"
+require "../hitables/bvh"
+
+class BucketInfo
+  property count : Int32
+  property bounds : AABB
+
+  def initialize
+    @count = 0
+    @bounds = AABB.new(Vec3::ZERO, Vec3::ZERO)
+  end
+end
+
+# A special kind of BVH,
+# trying to minimize the overlap of the left and right bounding boxes.
+# Construction takes longer but the resulting BVH is (probably) faster
+class SAHBVHNode < BVHNode
+  def initialize(list)
+    n = list.size
+    if n == 1
+      # This should never happen
+      @left = @right = list[0]
+    elsif n == 2
+      @left = list[0]
+      @right = list[1]
+    else
+      centroids = list.map { |obj| obj.bounding_box.centroid }
+      min = centroids.reduce(Vec3.new(Float64::MAX)) { |centroid, min| centroid.min(min) }
+      max = centroids.reduce(Vec3.new(-Float64::MAX)) { |centroid, max| centroid.max(max) }
+      delta = max - min
+
+      if delta.x >= delta.y && delta.x >= delta.z
+        axis = 0
+      elsif delta.y >= delta.x && delta.y >= delta.z
+        axis = 1
+      else
+        axis = 2
+      end
+
+      n_buckets = 12
+      buckets = Array.new(n_buckets) { BucketInfo.new }
+
+      # Generate buckets
+      sorted = list.sort_by { |h| h.bounding_box.centroid[axis] }
+      sorted.each do |object|
+        b = (n_buckets * (object.bounding_box.centroid[axis] - min[axis]) / (max[axis] - min[axis])).to_i
+        b = n_buckets - 1 if b == n_buckets
+
+        buckets[b].count += 1
+        if buckets[b].count == 1
+          buckets[b].bounds = object.bounding_box
+        else
+          buckets[b].bounds = buckets[b].bounds.merge(object.bounding_box)
+        end
+      end
+
+      cost = Array.new(n_buckets - 1, 0.0)
+
+      # Rate buckets
+      (0...(n_buckets - 1)).each do |i|
+        count0 = 0
+        count1 = 1
+
+        b0 = buckets[0].bounds
+        (0..i).each do |j|
+          b0 = b0.merge(buckets[j].bounds)
+          count0 += buckets[j].count
+        end
+
+        b1 = buckets[i+1].bounds
+        ((i+1)...n_buckets).each do |j|
+          b1 = b1.merge(buckets[j].bounds)
+          count1 += buckets[j].count
+        end
+
+        cost[i] = 0.125 + (count0*area(b0.min, b0.max) + count1*area(b1.min, b1.max)) / area(min, max)
+      end
+
+      min_cost = cost[0]
+      min_cost_split = 0
+
+      # Find best split
+      cost.each_with_index do |c, index|
+        if c < min_cost
+          min_cost = c
+          min_cost_split = index
+        end
+      end
+
+      left_ = [] of Hitable
+      right_ = [] of Hitable
+
+      # Perform split
+      sorted.each do |obj|
+        b = (n_buckets * (obj.bounding_box.centroid[axis] - min[axis]) / (max[axis] - min[axis])).to_i
+        b = n_buckets - 1 if b == n_buckets
+
+        if b <= min_cost_split
+          left_ << obj
+        else
+          right_ << obj
+        end
+      end
+
+      if left_.size == 1
+        @left = left_[0]
+      else
+        @left = SAHBVHNode.new(left_)
+      end
+
+      if right_.size == 1
+        @right = right_[0]
+      else
+        @right = SAHBVHNode.new(right_)
+      end
+    end
+
+    @bounding_box = @left.bounding_box.merge(@right.bounding_box)
+  end
+
+  def area(min, max)
+    delta = max - min
+    (delta.x * delta.y + delta.x * delta.z + delta.y * delta.z) * 2.0
+  end
+end
