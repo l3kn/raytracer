ce04f4f4219c3d089895469193acec96187f2ff2
Leon Rische <leon.rische@student.uni-siegen.de>
2016-09-12 20:51:54 +0200

    Extract Metaball class

--- scenes/metaball.cr
+++ scenes/metaball.cr
@@ -5,42 +5,18 @@ require "../src/distance_estimatables/*"
 
 mat = Metal.new(Vec3.new(0.8), 0.0)
 
-class BFMeta < DE::BruteForceDistanceEstimatable
-  def initialize
-    @points = [
-      {Vec3.new(1.0, -0.5, -0.2), 1.0},
-      {Vec3.new(0.0, 0.5, 0.2), 1.0},
-      {Vec3.new(-1.0, -0.5, 0.0), 1.0},
-    ]
-  end
+bfde = DE::Metaball.new(
+  [
+    {Vec3.new(1.0, -0.5, -0.2), 1.0},
+    {Vec3.new(0.0, 0.5, 0.2), 1.0},
+    {Vec3.new(-1.0, -0.5, 0.0), 1.0},
+  ],
+  3.4
+)
+hitables = BruteForceDistanceEstimator.new(mat, bfde, 5.0)
 
-  def inside?(pos)
-    potential = 0.0
-
-    @points.each do |p_i, r_i|
-      potential += r_i / (pos - p_i).squared_length
-    end
-
-    potential > 3.4
-  end
-
-  def normal(pos)
-    n = Vec3::ZERO
-    @points.each do |p_i, r_i|
-      a = -2.0 * r_i
-      b = p_i - pos
-      c = (p_i - pos).squared_length
-      n = n + b * (a / c) 
-    end
-
-    n.normalize
-  end
-end
-
-bfde = BFMeta.new
-hitables = BruteForceDistanceEstimator.new(mat, bfde, 10.0)
-
-width, height = {800, 400}
+# width, height = {800, 400}
+width, height = {1920, 1080}
 
 camera = Camera.new(
   look_from: Vec3.new(0.0, 0.0, 2.0),
---
+++ src/distance_estimatables/metaball.cr
@@ -0,0 +1,34 @@
+require "../vec3"
+require "../distance_estimatable"
+
+module DE
+  class Metaball < DE::BruteForceDistanceEstimatable
+    getter points : Array(Tuple(Vec3, Float64))
+    getter threshold : Float64
+
+    def initialize(@points, @threshold)
+    end
+
+    def inside?(pos)
+      potential = 0.0
+
+      @points.each do |p_i, r_i|
+        potential += r_i / (pos - p_i).squared_length
+      end
+
+      potential > @threshold
+    end
+
+    def normal(pos)
+      n = Vec3::ZERO
+      @points.each do |p_i, r_i|
+        a = -2.0 * r_i
+        b = p_i - pos
+        c = (p_i - pos).squared_length
+        n = n + b * (a / c) 
+      end
+
+      n.normalize
+    end
+  end
+end
