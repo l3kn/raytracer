5ea461a60ea6d45cc0ee6d4589c6ef36b1c933d3
Leon Rische <leon.rische@student.uni-siegen.de>
2016-09-30 17:02:06 +0200

    Fix scene

--- scenes/metaball.cr
+++ scenes/metaball.cr
@@ -3,24 +3,24 @@ require "../src/hitables/distance_estimator"
 require "../src/backgrounds/*"
 require "../src/distance_estimatables/*"
 
-mat = Metal.new(Vec3.new(0.8), 0.0)
+mat = Metal.new(Color.new(0.8), 0.0)
 
 bfde = DE::Metaball.new(
   [
-    {Vec3.new(1.0, -0.5, -0.2), 1.0},
-    {Vec3.new(0.0, 0.5, 0.2), 1.0},
-    {Vec3.new(-1.0, -0.5, 0.0), 1.0},
+    {Point.new(1.0, -0.5, -0.2), 1.0},
+    {Point.new(0.0, 0.5, 0.2), 1.0},
+    {Point.new(-1.0, -0.5, 0.0), 1.0},
   ],
   3.4
 )
 hitables = BruteForceDistanceEstimator.new(mat, bfde, 5.0)
 
-# width, height = {800, 400}
-width, height = {1920, 1080}
+width, height = {800, 400}
+# width, height = {1920, 1080}
 
 camera = Camera.new(
-  look_from: Vec3.new(0.0, 0.0, 2.0),
-  look_at: Vec3.new(0.0, 0.0, 0.0),
+  look_from: Point.new(0.0, 0.0, 2.0),
+  look_at: Point.new(0.0, 0.0, 0.0),
   vertical_fov: 70,
   aspect_ratio: width.to_f / height.to_f,
 )
