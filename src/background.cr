5a57eb2e104deb314130497e65622ccc2366474d
Leon Rische <leon.rische@student.uni-siegen.de>
2017-02-20 20:40:27 +0100

    Combine background files

--- src/background.cr
+++ src/background.cr
@@ -1,3 +1,19 @@
 abstract class Background
   abstract def get(ray)
 end
+
+class SkyBackground < Background
+  def get(ray)
+    t = 0.5 * (ray.direction.normalize.y + 1.0)
+    Color.new(0.5, 0.7, 1.0).mix(Color::WHITE, t)
+  end
+end
+
+class ConstantBackground < Background
+  def initialize(@color : Color)
+  end
+
+  def get(ray)
+    @color
+  end
+end
--- src/backgrounds/sky.cr
+++
@@ -1,8 +0,0 @@
-require "../background"
-
-class SkyBackground < Background
-  def get(ray)
-    t = 0.5 * (ray.direction.normalize.y + 1.0)
-    Color.new(0.5, 0.7, 1.0).mix(Color::WHITE, t)
-  end
-end
